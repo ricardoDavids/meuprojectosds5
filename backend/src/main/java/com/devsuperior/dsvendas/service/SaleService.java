25e16c0dd89accf85cedd4c31973980c850329bc
Ricardo David <ricardo.david2020.new@gmail.com>
2021-11-10 00:09:19 +0000

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsvendas.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly =true) // Isto aqui vai garantir que toda a operação com o banco seja resolvida neste momento aqui no service e o ReadOnly é para nao fazer lock no banco, como é uma operação só de leitura nao precisa de fazer lá locking de ecrita  
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale>result= repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
