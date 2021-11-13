d6f71d3666f55e3322e67351aa83137c93c0f93e
Ricardo David <ricardo.david2020.new@gmail.com>
2021-11-13 20:01:11 +0000

    Group By search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsvendas.repositories;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +9,6 @@ import com.devsuperior.dsvendas.entities.Seller;
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 
 	
-	List<Seller> findAll();
+	//List<Seller> findAll();
 
 }
