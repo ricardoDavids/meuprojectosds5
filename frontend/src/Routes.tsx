a7e80b3468ae0d5bd203643e2a53ecca78962139
Ricardo David <ricardo.david2020.new@gmail.com>
2021-11-15 23:55:46 +0000

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,27 @@
+
+
+import Dashboard from 'pages/Dashboard';
+import Home from 'pages/Home';
+import { Switch } from 'react-router-dom';
+//import { Switch } from 'react-router-dom';
+import {BrowserRouter ,Route } from 'react-router-dom';
+
+
+
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+               <Route path="/dashboard">
+                   <Dashboard/>
+               </Route>
+            </Switch>
+        </BrowserRouter>
+    ); 
+}
+
+export default Routes;
