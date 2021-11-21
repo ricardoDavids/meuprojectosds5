f6c95d63394684539317c054d6a31aedf3073f40
Ricardo David <ricardo.david2020.new@gmail.com>
2021-11-21 20:07:50 +0000

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,32 @@
+import { seller } from "./seller"
+
+export type Sale ={
+    id: number; 
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller : seller;
+    
+}
+//Aqui temos um objecto, um tipo que vai casar coom a resposta da busca Paginada do POSTMAN
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
+    
 }
 
 //Aqui no export type SaleSum foi definido um Tipo que tinhamos no postman
---
+++ frontend/src/types/seller.ts
@@ -0,0 +1,5 @@
+export type seller={
+    
+        id: number;
+        name: string;
+    }
